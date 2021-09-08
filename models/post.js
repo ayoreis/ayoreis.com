9ffa2b96098307b2b81e496a9086a959d7f6e51e
Ayo Reis <ayo.v.reis@gmail.com>
2021-09-08 13:26:07 +0100

    🍃 Created mongoDB Model & Schema.

---
+++ models/post.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    content: {
+        type: Sting,
+        required: true
+    }
+}, {timestamps: true})
+
+fs.readFile('./authentication.json', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+
+    authentication = JSON.parse(data)
+
+    const Post = mongoose.model(authentication.collection, postSchema) //
+
+    module.exports = postSchema
+})
