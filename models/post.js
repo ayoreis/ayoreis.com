9bd0225183ebcb3a1aabc0960bf3d051cad4fe7b
Ayo Reis <ayo.v.reis@gmail.com>
2021-09-08 13:38:13 +0100

    Added rigth URI to authentication.json

--- models/post.js
+++ models/post.js
@@ -24,7 +24,7 @@ const postSchema = new Schema({
     }
 }, {timestamps: true})
 
-fs.readFile('./authentication.json', (err, data) => {
+fs.readFile('../authentication.json', (err, data) => {
     if (err) {
         console.error(err)
         return
