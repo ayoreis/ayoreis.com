1163c5c7f4e9959f17a91e25190d3eae48e16936
Ayo Reis <ayo.v.reis@gmail.com>
2021-09-08 13:10:30 +0100

    📶 Conecting to mongoDB Atlas database

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const fs = require('fs')
 const mongoose = require('mongoose')
+
+let authentication, dbURI
+
+fs.readFile('./authentication.json', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+
+    authentication = JSON.parse(data)
+    dbURI = `mongodb+srv://${authentication.user}:${authentication.password}@cluster.alfss.mongodb.net/${authentication.database}?retryWrites=true&w=majority`
+
+    mongoose.connect(dbURI)
+})
+
+
 
 const app = express()
 
