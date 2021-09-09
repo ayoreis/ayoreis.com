b0d877bffe0b0f04071689327506b2a5a3df3f29
Ayo Reis <ayo.v.reis@gmail.com>
2021-09-09 16:10:56 +0100

    Simple working version.

--- app.js
+++ app.js
@@ -8,10 +8,19 @@ const mongoose = require('mongoose')
 const Post = require('./models/post')
 
 const app = express()
+
+
+
+// Settings
 app.set('view engine', 'ejs')
 
+// Middleware
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 
+
+
+// Routes
 app.get('/', (request, response) => {
     response.render('index', {title: "Ayo Reis."})
 })
@@ -20,11 +29,39 @@ app.get('/about', (request, response) => {
     response.render('about', {title: "About."})
 })
 
+
+
+// Post routes
+
 app.get('/post', (request, response) => {
     response.render('post', {title: "Post."})
 })
 
+app.get('/posts', (request, response) => {
+    Post.find().sort({ createdAt: -1 })
 
+    .then((result) => {
+        response.render('posts', {title: "Posts.", posts: result})
+    })
+
+    .catch(console.error)
+})
+
+app.post('/posts', (request, response) => {
+    const post = new Post(request.body)
+
+    post.save()
+
+    .then(() => {
+        response.redirect('/posts')
+    })
+
+    .catch(console.error)
+})
+
+
+
+// Database
 let authentication, databaseURI
 
 fs.readFile('./authentication.json', (err, data) => {
