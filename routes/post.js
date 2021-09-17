0cc3a84f3bc06c4b76b197563b668a4bfc41d636
Ayo Reis <ayo.v.reis@gmail.com>
2021-09-17 16:34:28 +0100

    ;Removed semicolon.

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const Post = require('../models/post')
 const router = express.Router()
-const sha512 = require('js-sha512');
+const sha512 = require('js-sha512')
 
 const config = JSON.parse(fs.readFileSync('./config.json'))
 
