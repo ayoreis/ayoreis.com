27bedcb79fe656893512a64f7e9308d031f2781d
Ayo Reis <ayo.v.reis@gmail.com>
2021-09-17 16:31:56 +0100

    🤏 Shortened code.

--- models/post.js
+++ models/post.js
@@ -22,6 +22,4 @@ const postSchema = new Schema({
     }
 }, {timestamps: true})
 
-const Post = mongoose.model('Post', postSchema)
-
-module.exports = Post
+module.exports = mongoose.model('Post', postSchema)
