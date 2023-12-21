4381acc8f376f7cdbc75b2b261ede51c5e3fc299
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2023-12-21 20:36:08 +0000

    I'm back baby! And with new stuff and all.

--- mod.ts
+++ mod.ts
@@ -1,18 +1,7 @@
-import { Router } from 'relax/router/router.ts';
-import { serve } from 'std/http/server.ts';
+const html = await Deno.readTextFile("index.html")
 
-import handleFormat  from './handle-format.ts';
-import handleIndex from './handle-index.ts';
-import handleNotFound from './handle-not-found.ts';
-import handlePage from './handle-page.ts';
-import handleStaticFile from './handle-static-file.ts';
-
-const router = new Router();
-
-router.get('*', handleFormat);
-router.get('/', handleIndex);
-router.get('*', handleStaticFile);
-router.get('/:id', handlePage);
-router.get('*', handleNotFound);
-
-serve(router.fetch);
+Deno.serve(() => {
+	return new Response(html, {
+		headers: { "Content-Type": "text/html" },
+	})
+})
