5f0b637b4c77286b77c6e4d333c5a46080e2d572
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2024-01-26 20:45:35 +0000

    88x31

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# [ayoreis.com](//ayoreis.com) <- My website
+# [ayoreis.com](//ayoreis.com)
+
+My website
+
+## Licence
+
+- Code: MIT
+- Content: None (all rights reserved)
+- Arch button: CC0
--- mod.ts
+++ mod.ts
@@ -1,17 +1,24 @@
 import { Status } from 'std/http/http_status.ts'
+import { serveDir } from 'std/http/file_server.ts'
 import { parse } from 'npm:marked'
 
 const html = await Deno.readTextFile('index.html')
 const markdown = await Deno.readTextFile('index.md')
 const result = html.replace('{markdown}', await parse(markdown))
 
-function handler({ url }: Request) {
-	const { pathname } = new URL(url)
+async function handler(request: Request) {
+	const { pathname } = new URL(request.url)
+
+	console.log(pathname)
 
 	if (pathname === '/') {
 		return new Response(result, {
 			headers: { 'Content-Type': 'text/html' },
 		})
+	}
+
+	if (pathname.startsWith('/static')) {
+		return await serveDir(request)
 	}
 
 	return new Response(null, { status: Status.NotFound })
